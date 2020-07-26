5574565de4c8fe3c928fc14feddb2371d1f92993
David Brady <davidjbrady97@gmail.com>
2020-07-25 21:49:52 -0700

    update prod settings

--- settings/production.py
+++ settings/production.py
@@ -1,6 +1,6 @@
 from settings.base import *
 
-ALLOWED_HOSTS = ["pawnhall.pythonanywhere.com"]
+ALLOWED_HOSTS = ["www.pawnguild.xyz"]
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
