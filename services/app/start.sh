1909989ac300f3d745d89635e1c7746ef89240f6
David Brady <davidjbrady97@gmail.com>
2021-12-13 22:54:17 +0000

    nginx in

--- services/app/start.sh
+++ services/app/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo 'Collecting static files...'
-python manage.py collectstatic --no-input
+# echo 'Collecting static files...'
+# python manage.py collectstatic --no-input
 
-echo 'Creating migration files...'
-python manage.py makemigrations
+# echo 'Creating migration files...'
+# python manage.py makemigrations
 
-echo 'Migrating database...'
-python manage.py migrate
+# echo 'Migrating database...'
+# python manage.py migrate
 
 echo 'Booting up server'
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
