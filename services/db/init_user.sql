30752698d7a0fad1dd4feca5b310126c3b25c609
David Brady <davidjbrady97@gmail.com>
2021-12-16 01:06:20 +0000

    dev docker compose file written. automatically creates superuser if it doesnt exist

--- services/db/init_user.sql
+++ services/db/init_user.sql
@@ -1,7 +1,7 @@
 CREATE USER pawnguild_app WITH PASSWORD 'm4KJvXJ/iLbMLcxn47ZwZDBP0jaH+zzf';
 
--- ALTER ROLE pawnguild_app SET client_encoding TO 'utf8';
--- ALTER ROLE pawnguild_app SET default_transaction_isolation TO 'read committed';
--- ALTER ROLE pawnguild_app SET timezone TO 'UTC';
+ALTER ROLE pawnguild_app SET client_encoding TO 'utf8';
+ALTER ROLE pawnguild_app SET default_transaction_isolation TO 'read committed';
+ALTER ROLE pawnguild_app SET timezone TO 'UTC';
 
 GRANT ALL PRIVILEGES ON DATABASE pawnguild TO pawnguild_app;
