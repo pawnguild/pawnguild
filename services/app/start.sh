4a873da5e0e814ab4f0b9bc3d9af23bf27286fdd
DavidJayBrady <davidjbrady97@gmail.com>
2022-04-22 21:31:55 -0700

    runs well locally

--- services/app/start.sh
+++ services/app/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # echo 'Collecting static files...'
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # echo 'Creating migration files...'
-# python manage.py makemigrations
+python manage.py makemigrations
 
 # echo 'Migrating database...'
-# python manage.py migrate
+python manage.py migrate
 
 # echo 'Creating superuser...'
-# python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput
 
 echo 'Booting up server'
 gunicorn config.wsgi:application --bind 0.0.0.0:${DJANGO_PORT}
