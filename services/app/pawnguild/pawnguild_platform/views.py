d91b4a6e045de2275e65e23f560abee734d72394
David Brady <djbrady@uci.edu>
2022-05-02 17:56:09 -1000

    formatted

--- services/app/pawnguild/pawnguild_platform/views.py
+++ services/app/pawnguild/pawnguild_platform/views.py
@@ -3,13 +3,11 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import View
 from django.shortcuts import render
 
+
 class Home(View):
-    
     def get(self, request):
         context = {}
-        return render(
-            request, "pawnguild_platform/bis_home.html", context=context
-        )
+        return render(request, "pawnguild_platform/bis_home.html", context=context)
 
 
 class FAQ(TemplateView):
