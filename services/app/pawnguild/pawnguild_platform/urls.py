1263e33c55ac6afbc2df2bb724de5037d78c3838
David Brady <djbrady@uci.edu>
2022-05-04 15:28:40 -1000

    giving up on home page

--- services/app/pawnguild/pawnguild_platform/urls.py
+++ services/app/pawnguild/pawnguild_platform/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.Home.as_view(), name="home"),
+    # path("", views.Home.as_view(), name="home"),
     path("faq/", views.FAQ.as_view(), name="faq"),
 ]
--- services/app/pawnguild/pawnguild_platform/views.py
+++ services/app/pawnguild/pawnguild_platform/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 class Home(View):
     def get(self, request):
         context = {}
-        return render(request, "pawnguild_platform/home.html", context=context)
+        return render(request, "pawnguild_platform/bis_home.html", context=context)
 
 
 class FAQ(TemplateView):
